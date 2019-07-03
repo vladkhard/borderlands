8c8c2a1a7806b81a998153a2382ddf813a4e9de2
vladkhard <vladkhard@gmail.com>
2019-07-03 09:43:57 +0300

    Migrate to mongo

--- application/settings.py
+++ application/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': os.getenv('DB_NAME', 'citizens-db'),
     }
 }
 
